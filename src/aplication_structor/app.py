7614544fa8c2d1f9e9614c8fce01dfc380d1f24b
gayane <gaya.mejlumyan@gmail.com>
2018-06-02 20:44:35 +0400

    aplication structure

---
+++ src/aplication_structor/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from config import conf
+# from com.auth.route import auth
+# base
+# db  connection
+# 
+
+app = Flask(__name__)
+
+# register blueprints for components
+# app.register_blueprint(auth, url_prefix='/auth')
+
+
+if __name__ == '__main__':
+    app.run(conf['general']['HOST'], conf['general'].getint('PORT'), conf['general']['DEBUG'])
