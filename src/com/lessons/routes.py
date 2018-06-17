887fa7a76b06b6ce589a0d41c6b83befab64e897
gayane <gaya.mejlumyan@gmail.com>
2018-06-17 16:29:29 +0400

    lessons controllers rename

--- src/com/lessons/routes.py
+++ src/com/lessons/routes.py
@@ -1,9 +1,9 @@
 from com.auth.routes import login_required
 from flask import Blueprint, render_template, redirect, request, url_for, flash, send_from_directory
-from com.lessons.controllers import LessonsController
+from com.lessons.controller import LessonsController
 from os import path
 
-from com.quiz.quiz_controller import QuizController
+from com.quiz.controller import QuizController
 
 lessons = Blueprint("lessons", __name__, template_folder="templates", static_folder="static",
                     url_prefix="/lessons")
